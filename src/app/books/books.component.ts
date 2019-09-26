7db4fbcb23af9045807fbf9e497c220c53f11d9b
Brooke Taylor Isaacson <nel13012@byui.edu>
2019-09-26 12:15:56 -0600

    adding dependencies for Heroku to deploy

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Book } from '../Book';
+import { Book } from '../book';
 import { BookService } from '../book.service';
 
 @Component({
